d547d318f6c89857cfb68cb4d87b70bf397176d8
Elchanan5760 <emstone@gmail.com>
2025-09-08 09:28:04 +0300

    unique id

--- services/persister/per_manager.py
+++ services/persister/per_manager.py
@@ -1,10 +1,13 @@
 from services.kafka.sub import Sub
+from unique_id import CreateID
 
 class PersisterManager:
     def __init__(self):
         self.sub = Sub("processed")
         self.sub.connect()
+        self.create_id = CreateID()
     def manage_all_files(self):
         data = self.sub.sub()
         for msg in data:
             print(msg)
+            print(self.create_id.hashing(msg))
---
+++ services/persister/unique_id.py
@@ -0,0 +1,11 @@
+import hashlib
+
+class CreateID:
+    def __init__(self):
+        self.hasher = hashlib.sha256()
+
+    def hashing(self,data_dict):
+        sorted_items = sorted(data_dict.items())
+        dict_string = repr(sorted_items).encode('utf-8')
+        self.hasher.update(dict_string)
+        return self.hasher.hexdigest()
