c8b1370054ee6063c1241d10777c81cef9e2ed4c
Elchanan5760 <emstone@gmail.com>
2025-09-10 17:19:44 +0300

    bds percentage calculation done

--- services/classifier/classifier_manager.py
+++ services/classifier/classifier_manager.py
@@ -1,26 +1,34 @@
 import os
-
-from services.utils.elastic.search_query import Queries
-from risk_calculate import RiskCalculation
+from services.utils.elastic.dal import DAL
+from services.utils.elastic.queries import Queries
+from services.classifier.calculation.risk_calculate import RiskCalculation
 from services.utils.files_access.text_access import Text
 from services.utils.encryption.encryption import Encryption
+from services.utils.elastic.logger import Logger
 
 class ClassifierManager:
     def __init__(self):
-        self.search_query = Queries()
+        self.logger = Logger.get_logger()
+        self.es_dal = DAL()
+        self.queries = Queries()
         self.risk_calc = RiskCalculation()
         self.FILE_PATH = os.getenv("FILE_PATH",r"C:\Users\HOME\PycharmProjects\The_Muezzin\data\hostile.txt")
         self.text = Text(self.FILE_PATH)
         self.encription = Encryption()
 
     def manage_all_files(self):
-        list_id = self.search_query.get_all_id()
-        for doc_id in list_id:
-            encrypt_words = self.text.organized_txt()
-            decrypt_hostile = str(self.encription.decrypt(encrypt_words["hostile"]))[2:].split(',')
-            hostile_percentage = self.risk_calc.bds_percent(doc_id,"transcribed", decrypt_hostile)
-            print(hostile_percentage)
-            decrypt_less_hostile = str(self.encription.decrypt(encrypt_words["less hostile"]))[2:].split(',')
-            less_hostile_percentage = self.risk_calc.bds_percent(doc_id,"transcribed", decrypt_less_hostile)
-            print(less_hostile_percentage['words_persent'] / 2)
-            final_result = less_hostile_percentage['words_persent'] / 2 + hostile_percentage['words_persent']
+        self.logger.info("Classifier started.")
+        doc_list = self.queries.get_all_data()
+        encrypt_words = self.text.organized_txt()
+        decrypt_hostile = str(self.encription.decrypt(encrypt_words["hostile"]))[2:].split(',')
+        decrypt_less_hostile = str(self.encription.decrypt(encrypt_words["less hostile"]))[2:].split(',')
+        for doc in doc_list:
+            hostile_percentage = self.risk_calc.bds_percent(doc['_source']["transcribed"], decrypt_hostile)
+            less_hostile_percentage = self.risk_calc.bds_percent(doc['_source']["transcribed"], decrypt_less_hostile)/2
+            percent = hostile_percentage + less_hostile_percentage
+            self.es_dal.update_doc(doc['_id'],"bds_percent",percent)
+            is_bds = self.risk_calc.is_bds(percent)
+            self.es_dal.update_doc(doc['_id'], "is_bds", is_bds)
+            bds_treat_level = self.risk_calc.bds_threat_level(percent)
+            self.es_dal.update_doc(doc['_id'], "bds_treat_level", bds_treat_level)
+            print(self.queries.get_all_data())
